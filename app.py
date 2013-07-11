758cc8fae69acab75739888ea2107497de442aef
David Fioretti <fioretti.david@gmail.com>
2013-07-10 22:55:31 -0400

    update api

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask.ext.pymongo import PyMongo
+from flask.ext.restful import Resource, Api
 
-
+# app configuration
 app = Flask(__name__)
+
+api = Api(app)
+
 mongo = PyMongo(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return { 'hello' : 'world' }
+
+api.add_resource(HelloWorld, '/api')
 
 
 
