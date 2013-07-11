9e3b1b1ded374b70c8fee6dabc7871b6af5491c8
David Fioretti <fioretti.david@gmail.com>
2013-07-11 00:22:45 -0400

    progress

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
 from flask import Flask, jsonify, request
+from functools import wraps
 from flask.ext.pymongo import PyMongo
-from flask.ext.restful import Resource, Api
+from flask.ext.restful import Resource, Api, reqparse
 
 # app configuration
 app = Flask(__name__)
 
-api = Api(app)
+api = Api(app=app, prefix='/api/v1')
 
 mongo = PyMongo(app)
 
-class HelloWorld(Resource):
+# TODO build auth
+def basic_authetication():
+    return True
+
+def authenticate(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if not getattr(func, 'authenticated', True):
+            return func(*args, **kwargs)
+        acct = basic_authetication()
+        if acct:
+            return func(*args, **kwargs)
+        abort(401)
+    return wrapper
+
+class Merchant(Resource):
+    def get(self, merchant_id):
+        return mongo.db.merchants.find( { 'merchant_id' : merchant_id } )
+
+    def delete(self, merchant_id):
+        mongo.db.merchants.remove( { 'merchant_id' : merchant_id } )
+
+    def put(self, merchant_id):
+        args = parser.parse_args()
+        merchant = { 'merchant' : args['merchant'] }
+        mongo.db.merchants.save( merchant )
+
+class Merchants(Resource):
     def get(self):
+        return mongo.db.merchants.find()
+
+
+class HelloWorld(Resource):
+    method_decorators = [authenticate]
+    def get(self):
+        # for reference
+        app.logger.info('informing')
+        app.logger.warning('warning')
+        app.logger.error('screaming bloody murder!')
         return { 'hello' : 'world' }
 
-api.add_resource(HelloWorld, '/api')
+
+
+api.add_resource( HelloWorld, '/' )
+api.add_resource( Merchant, '/merchant/<string:merchant_id>' )
+api.add_resource( MerchantList, '/merchants' )
+
 
 
 
@@ -29,3 +72,7 @@ def test():
 
 if __name__ == '__main__':
     app.run(debug = True, port=5001)
+
+# resource
+# http://stackoverflow.com/questions/739654/how-can-i-make-a-chain-of-function-decorators-in-python
+# https://github.com/twilio/flask-restful/issues/18 multiple files blueprint
