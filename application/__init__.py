c19717ff0226116716a61d681898f0a19d46d95f
David Fioretti <fioretti.david@gmail.com>
2013-07-23 23:47:42 -0400

    new boilerplate

---
+++ application/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from application.config import DevelopmentConfig
+from application.config import ProductionConfig
+from flask.ext import restful
+from flask.ext.pymongo import PyMongo
+import os
+
+# create the main Flask app
+app = Flask(__name__)
+if 'APP_ENV' in os.environ and os.environ['APP_ENV'] == 'production':
+    app.config.from_object(ProductionConfig)
+else:
+    app.config.from_object(DevelopmentConfig)
+
+# connect to the database
+#db = MongoEngine(app)
+db = PyMongo(app)
+
+# setup restful support
+api = restful.Api(app)
+
+# load routes
+import routes
