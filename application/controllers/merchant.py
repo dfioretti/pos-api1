f6cba18c85e769556f62539859f52a2d8d923487
David Fioretti <fioretti.david@gmail.com>
2013-07-29 20:25:42 -0400

    two api calls working

--- application/controllers/merchant.py
+++ application/controllers/merchant.py
@@ -7,28 +7,28 @@ from flask.ext.restful import abort
 
 import json
 from bson import json_util
+from bson.objectid import ObjectId
 
 
-
-def json_fmt(cursor):
-    json_docs = []
-    for doc in cursor:
-        json_doc = json.dumps(doc, default=json_util.default)
-        json_docs.append(json_doc)
-    return json_docs
+def to_json(data):
+    return json.dumps(data, default=json_util.default)
 
 
 class Merchant(restful.Resource):
-    def get(self):
-        mongo.db.tests.save(test)
-        return {
-            'merchant': True
-        }
+    def get(self, merchant_id):
+        app.logger.info(merchant_id)
+        oid = ObjectId(merchant_id)
+        return to_json(mongo.db.merchants.find_one( { '_id' : oid } ))
+        #return mongo.db.merchants.find_one( { '_id' : ObjectId(merchant_id) } )
+
 
 class MerchantList(restful.Resource):
     def get(self):
-        cursor = mongo.db.merchants.find()
-        return json_fmt(cursor)
+        results = mongo.db.merchants.find()
+        json_result = []
+        for result in results:
+            json_result.append(result)
+        return to_json(json_result)
 
 #
 # name, street, city, state, zip, phone
